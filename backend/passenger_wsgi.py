6d9271dc955de712c6e3f9b004e6a4898d47aade
Ujjwal <ujjwalm9895@gmail.com>
2026-01-21 16:09:44 +0530

    done

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,8 +1,11 @@
-import os, sys
+import os
+import sys
 
+# Add application root to Python path
 sys.path.insert(0, os.path.dirname(__file__))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+# Set Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
