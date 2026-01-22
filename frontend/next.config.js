73a23302bd4161c82c3616e75b7960e496f69b7a
Ujjwal <ujjwalm9895@gmail.com>
2026-01-23 01:02:25 +0530

    build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,33 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // ✅ REQUIRED for standalone build (Docker)
-  output: "standalone",
-
-  images: {
-    unoptimized: true,
-    domains: ["localhost", "127.0.0.1", "storage.googleapis.com"],
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-      },
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-      },
-    ],
-  },
-
-  // ✅ use NEXT_PUBLIC_ for frontend
-  env: {
-    // NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000", // Not needed with Server Actions
-  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
